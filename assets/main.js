80fed662b25a3c2060a28285759542efcbb570ea
Alejaldo <statfluct@yahoo.com>
2021-04-22 18:07:08 +0500

    Initial commit

---
+++ assets/main.js
@@ -0,0 +1,23 @@
+const ytForm = document.getElementById('yt-form');
+const ytQuery = document.getElementById('yt-query');
+const vmForm = document.getElementById('vm-form');
+const vmQuery = document.getElementById('vm-query');
+const youtube = 'https://www.youtube.com/results?search_query=';
+const vimeo = 'https://vimeo.com/search?q=';
+
+function ytSubmitted(event) {
+  event.preventDefault();
+  const url = youtube + ytQuery.value;
+  const win = window.open(url, '_blank', 'noopener,resizable,scrollbars');
+  win.focus();
+}
+
+function vmSubmitted(event) {
+  event.preventDefault();
+  const url = vimeo + vmQuery.value;
+  const win = window.open(url, '_blank', 'noopener,resizable,scrollbars');
+  win.focus();
+}
+
+ytForm.addEventListener('submit', ytSubmitted);
+vmForm.addEventListener('submit', vmSubmitted);
